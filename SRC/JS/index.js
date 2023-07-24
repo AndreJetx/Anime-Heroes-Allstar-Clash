71a0b64ca9f24501ab4bc3dfcc93ebeeef683639
Andrjetx <139168174+Andrjetx@users.noreply.github.com>
2023-07-24 17:53:57 +0000

    -m "corrigindo GIF"

--- SRC/JS/index.js
+++ SRC/JS/index.js
@@ -30,7 +30,7 @@ personagens.forEach((personagem) => {
       const imagemPersonagemGrande = document.querySelector('.personagem-grande');
             
             const idPersonagem = personagem.attributes.id.value
-            imagemPersonagemGrande.src = `./src/img/gif-dbz/card-${idPersonagem}.gif`; 	
+            imagemPersonagemGrande.src = `/src/img/gif-dbz/card-${idPersonagem}.gif`; 	
 
             const nomePesonagem = document.getElementById('nome-personagem');
             nomePesonagem.innerText = personagem.getAttribute('data-name');
